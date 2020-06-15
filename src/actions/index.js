610923cc140e857293eb992f9c064226ad3db5be
alphadzingina <alphaadx6@gmail.com>
2020-06-15 05:01:02 +0100

    refactor fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,9 +14,14 @@ export const fetchPostsAndUsers = () => {
     return async (dispatch, getState) => {
         await dispatch(fetchPosts())
 
-        const userIds = _.uniq(_.map( getState().posts, 'userId'));
+        //const userIds = _.uniq(_.map( getState().posts, 'userId'));
+        //userIds.forEach(id => dispatch(fetchUser(id)));
 
-        userIds.forEach(id => dispatch(fetchUser(id)));
+        _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
     };
 };
 
